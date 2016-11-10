320f80210d5c1e9cb763f043d7df30bfb9b0b676
Kevin Raneri <kevin.raneri@gmail.com>
2016-11-09 19:37:15 -0500

    Added beacon detection to autonomous, and adjusted color sensorthreshold.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFindWhiteLine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoFindWhiteLine.java
@@ -24,7 +24,8 @@ public class AutoFindWhiteLine extends LinearOpMode {
             telemetry.update();
         }*/
 
-        while (AutonomousUtils.getLightSensorData(0).getData() < 0.27) {
+
+        while (AutonomousUtils.getLightSensorData(0).getData() < 0.33) {
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
@@ -35,6 +36,17 @@ public class AutoFindWhiteLine extends LinearOpMode {
         hardware.right.setPower(0);
         hardware.left.setPower(0);
 
+        ColorDetectionThread colorDetectionThread = new ColorDetectionThread(1.5f, 0f, 0f, 0f);
+        Thread t = new Thread(colorDetectionThread);
+        t.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        BeaconState state = colorDetectionThread.getState();
+        telemetry.addData("State", state);
+        telemetry.update();
 
     }
 
