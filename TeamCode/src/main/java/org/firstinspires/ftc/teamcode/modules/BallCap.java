28dab29bb1ba11867b6b682f5fcda9e251ed5f0a
Kevin Raneri <kevin.raneri@gmail.com>
2017-02-03 16:47:53 -0500

    Fully removed a legacy statement in BallCap that prevented the arm from being lifted, and was temporarily removed previously.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/BallCap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/BallCap.java
@@ -47,54 +47,50 @@ public class BallCap extends Module {
             holdEnabled = false;
         }
 
+        if (enabled) {
+            if (holdEnabled) {
+                hardware.ballcaphold.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                hardware.ballcaphold.setTargetPosition(Globals.ballcapholdEnabled);  //set it to go to clamp
+                if(hardware.ballcaphold.getCurrentPosition() > Globals.ballcapholdSlowZone){ //if it's far enough, apply more powah!
+                    hardware.ballcaphold.setPower(Globals.ballcapclampSpeed);
+                }else{
+                    hardware.ballcaphold.setPower(Globals.ballcapholdSpeed);
+                }
 
-        if (enabled || true) {
-
-            if (enabled) {
-                if (holdEnabled) {
-                    hardware.ballcaphold.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    hardware.ballcaphold.setTargetPosition(Globals.ballcapholdEnabled);  //set it to go to clamp
-                    if(hardware.ballcaphold.getCurrentPosition() > Globals.ballcapholdSlowZone){ //if it's far enough, apply more powah!
-                        hardware.ballcaphold.setPower(Globals.ballcapclampSpeed);
-                    }else{
-                        hardware.ballcaphold.setPower(Globals.ballcapholdSpeed);
-                    }
-
+            } else {
+                hardware.ballcaphold.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                if(hardware.ballcaphold.getCurrentPosition()>100){
+                    hardware.ballcaphold.setPower(-.15f);
                 } else {
-                    hardware.ballcaphold.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                    if(hardware.ballcaphold.getCurrentPosition()>100){
-                        hardware.ballcaphold.setPower(-.15f);
-                    } else {
-                        hardware.ballcaphold.setPower(0);
-                    }
+                    hardware.ballcaphold.setPower(0);
                 }
-            } else {
-                hardware.ballcaphold.setPower(0);
             }
-
-            if (teleop.getGamepad()[2].a) {
-                hardware.release.setPosition(Globals.releaseEnabled);
-                enabled = true;
-            } else {
-                hardware.release.setPosition(Globals.releaseDisabled);
-            }
-
-            if (Math.abs(teleop.getGamepad()[2].right_stick_y) >= .1) {
-                speed = teleop.getGamepad()[2].right_stick_y;
-                if (speed < -.5) { //the user needs to push the stick further than halfway in order ot unravel
-                    speed= -.10f; //when using steel cable, this needs to run slowly to uravel the  cable
-                }else if (speed < 0){ //if the stick is not all the way pressed, this executes, coasting
-                    speed = 0; //this just induces coasting as standard
-                }
-                hardware.ballcapper.setPower(speed);
-                hardware.lock.setPosition(Globals.lockDisabled);
-            } else {
-
-                hardware.ballcapper.setPower(0);
-                hardware.lock.setPosition(Globals.lockEnabled);
-            }
-
-
+        } else {
+            hardware.ballcaphold.setPower(0);
         }
+
+        if (teleop.getGamepad()[2].a) {
+            hardware.release.setPosition(Globals.releaseEnabled);
+            enabled = true;
+        } else {
+            hardware.release.setPosition(Globals.releaseDisabled);
+        }
+
+        if (Math.abs(teleop.getGamepad()[2].right_stick_y) >= .1) {
+            speed = teleop.getGamepad()[2].right_stick_y;
+            if (speed < -.5) { //the user needs to push the stick further than halfway in order ot unravel
+                speed= -.10f; //when using steel cable, this needs to run slowly to uravel the  cable
+            }else if (speed < 0){ //if the stick is not all the way pressed, this executes, coasting
+                speed = 0; //this just induces coasting as standard
+            }
+            hardware.ballcapper.setPower(speed);
+            hardware.lock.setPosition(Globals.lockDisabled);
+        } else {
+
+            hardware.ballcapper.setPower(0);
+            hardware.lock.setPosition(Globals.lockEnabled);
+        }
+
+
     }
 }
