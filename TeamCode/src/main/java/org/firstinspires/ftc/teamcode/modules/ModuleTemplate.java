4e7898955366e31983a8f3d397d25c6bec9d2f48
Kevin Raneri <kevin.raneri@gmail.com>
2017-02-15 19:01:01 -0500

    Created ModuleTemplate class for creating modules easier.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/ModuleTemplate.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+import org.firstinspires.ftc.teamcode.Teleop;
+
+/**
+ * Created by Kevin on 10/19/2016.
+ */
+public class ModuleTemplate extends Module {
+    public ModuleTemplate(Teleop t) {
+        super(t);
+    }
+
+    @Override
+    public void init() {}
+
+    @Override
+    public void initLoop() {}
+
+    @Override
+    public void start() {}
+
+    @Override
+    public void loop() {}
+
+    @Override
+    public void stop() {}
+}
