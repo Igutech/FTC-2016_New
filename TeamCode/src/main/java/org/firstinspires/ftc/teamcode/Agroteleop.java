ac2954adf0b64ab3d524e480f91e2a84f8b17c37
Tilman G <tilman@igutech.org>
2017-03-27 21:17:47 -0400

    Agrabot teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Agroteleop.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * This file provides basic Telop driving for a Pushbot robot.
+ * The code is structured as an Iterative OpMode
+ *
+ * This OpMode uses the common Pushbot hardware class to define the devices on the robot.
+ * All device access is managed through the HardwarePushbot class.
+ *
+ * This particular OpMode executes a basic Tank Drive Teleop for a PushBot
+ * It raises and lowers the claw using the Gampad Y and A buttons respectively.
+ * It also opens and closes the claws slowly using the left and right Bumper buttons.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+
+@TeleOp(name="Teleop Tank", group="Igutech")
+public class Agroteleop extends OpMode{
+
+
+
+    @Override
+    public void init() {
+        telemetry.addData("Say", "Hello Driver");    //
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+
+        // Run wheels in tank mode (note: The joystick goes negative when pushed forwards, so negate it)
+
+        hardwareMap.dcMotor.get("left").setPower(-gamepad1.left_stick_y);
+        hardwareMap.dcMotor.get("right").setPower(-gamepad1.right_stick_y);
+
+    }
+
+    @Override
+    public void stop() {
+    }
+
+}
